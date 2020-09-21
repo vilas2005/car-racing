5e2789fe0a8314fffe7bab8fc1534da500da1dc0
vilas2005 <68593708+vilas2005@users.noreply.github.com>
2020-09-21 19:47:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var hynoticBall,database,position;
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+
+    createCanvas(500,500);
+    hynoticBall = createSprite(250,250,10,10);
+    hynoticBall.shapeColor = "red";
+    var hynoticBallPosition = database.ref('ball/position');
+    hynoticBallPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(position !==undefined){
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x ,
+        'y': position.y + y
+    })
+    
+
+}
+function readPosition(data){
+    position = data.val();
+    console.log(position.x);
+    hynoticBall.x = position.x;
+    hynoticBall.y = position.y;
+    
+}
+function showError(){
+    console.log("error in data base")
+}
